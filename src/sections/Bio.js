7218e8a754939637ce824eee97d086980a085cdb
Devon Martin <devmart10@gmail.com>
2021-02-01 22:19:47 -0800

    add bio content

--- src/sections/Bio.js
+++ src/sections/Bio.js
@@ -5,14 +5,46 @@ const Bio = () => {
     <section id='bio' className='my-8 text-lg text-platinum'>
       <h2 className=''>Bio</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quisquam illum reprehenderit quidem doloremque esse eaque suscipit magnam placeat fuga praesentium vero quasi alias iusto ipsum
-        recusandae id quaerat deleniti numquam, ad impedit porro voluptas. Cum aut fugiat reprehenderit sit voluptatum odio, voluptatem excepturi error. Consequuntur iusto excepturi iste repellat!
+        <strong>Genki</strong>
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione non vel perferendis officiis error adipisci tempora, consequuntur quibusdam quam. Dolor optio laboriosam, ratione accusantium
-        numquam quod aliquid pariatur ipsa repellat soluta sed consequatur voluptatem! Omnis.
+        <span>Adjective</span>
       </p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed et voluptatem eius ad quas. Ipsam illo culpa officiis doloremque saepe.</p>
+
+      <p>
+        <em>Japanese&nbsp;</em>
+        <span>- lively, full of energy</span>
+      </p>
+      <p>
+        <em>&ldquo;Have you heard that new band Test The Waters? They are genki af!&rdquo;</em>
+      </p>
+
+      <p>
+        <span>We are Test The Waters, a pop punk band from the Bay Area. Our music is energetic, fast-paced, and above all fun.</span>
+      </p>
+
+      <p>
+        <span>
+          Test The Waters consists of TJ Tamura on vocals/bass guitar, Devon Martin on lead guitar, Cole Twitchell on rhythm guitar, and Ryan Bouquet on drums. In 2014 we all met in our freshman year
+          dorms at Cal Poly SLO. The die of fate rolled a nat 20, as TJ, Devon, and Cole were all roommates. After paying the ultimate blood price, Ryan was inducted into the circle of roommates as
+          well. The bond of friendship remained throughout college and lingered after graduation when TJ left for a two year teaching gig in Japan.
+        </span>
+      </p>
+
+      <p>
+        <span>
+          And then, a message from across the Pacific, heralded by angels and carried on wings of an eagle. TJ wanted to create a pop punk band and, like Jesus to the virgin Mary, Test the Waters was
+          born. Our initial demos were written by sending song files back and forth until we were eventually able to close the distance and record together.
+        </span>
+      </p>
+
+      <p>
+        <span>&ldquo;5% skill, 95% energy&rdquo; - NY Times</span>
+      </p>
+
+      <p>
+        <span>Our debut single &ldquo;On The Fence&rdquo; releases January 1st, 2021!</span>
+      </p>
     </section>
   );
 };
