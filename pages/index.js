f697908188607e386421d68b457bfd4dca84ed68
Devon Martin <devmart10@gmail.com>
2021-02-01 21:55:41 -0800

    adds banner ;)

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Layout from '../src/components/Layout';
 
 import AboutMe from '../src/sections/AboutMe';
+import Banner from '../src/sections/Banner';
 import Contact from '../src/sections/Contact';
 
 const Index = () => {
   return (
     <Layout>
-      {/* <AboutMe></AboutMe> */}
-      {/* <Contact></Contact> */}
+      <Banner></Banner>
     </Layout>
   );
 };
---
+++ src/sections/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <section id='banner'>
+      <iframe
+        width='1280'
+        height='720'
+        src='https://www.youtube.com/embed/dQw4w9WgXcQ'
+        frameborder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        allowfullscreen
+      ></iframe>
+    </section>
+  );
+};
+
+export default Banner;
