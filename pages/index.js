ae023fcd7ef4dc3371a795d256447795594cdd90
Devon Martin <devmart10@gmail.com>
2021-02-01 21:59:49 -0800

    adds bio

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Layout from '../src/components/Layout';
 
 import AboutMe from '../src/sections/AboutMe';
 import Banner from '../src/sections/Banner';
+import Bio from '../src/sections/Bio';
 import Contact from '../src/sections/Contact';
 
 const Index = () => {
   return (
     <Layout>
       <Banner></Banner>
+      <Bio></Bio>
     </Layout>
   );
 };
---
+++ src/sections/Bio.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Bio = () => {
+  return (
+    <section id='bio' className='my-8 text-platinum'>
+      <h2 className=''>Bio</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quisquam illum reprehenderit quidem doloremque esse eaque suscipit magnam placeat fuga praesentium vero quasi alias iusto ipsum
+        recusandae id quaerat deleniti numquam, ad impedit porro voluptas. Cum aut fugiat reprehenderit sit voluptatum odio, voluptatem excepturi error. Consequuntur iusto excepturi iste repellat!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione non vel perferendis officiis error adipisci tempora, consequuntur quibusdam quam. Dolor optio laboriosam, ratione accusantium
+        numquam quod aliquid pariatur ipsa repellat soluta sed consequatur voluptatem! Omnis.
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed et voluptatem eius ad quas. Ipsam illo culpa officiis doloremque saepe.</p>
+    </section>
+  );
+};
+
+export default Bio;
